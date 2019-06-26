760a1ebe0bf2ee50ca36605eb9e17aca75e32816
Yash Rastogi <yashrastogi@gmail.com>
2019-06-26 18:56:04 +0530

    ex6: Gauss. Kernel, Optim. Param.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+preError = ones(1, 3);
 
+for i=1:length(values),
+    for j=1:length(values),
+        C = values(i);
+        sigma = values(j);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        temp = ones(1, 3);
+        temp(1, 1) = C;
+        temp(1, 2) = sigma;
+        temp(1, 3) = mean(double(predictions ~= yval));
+        if temp(1,3) < preError(1,3),
+            preError = temp;
+        end
+    end
+end
 
-
-
-
+C = preError(1,1);
+sigma = preError(1,2);
 
 % =========================================================================
 
